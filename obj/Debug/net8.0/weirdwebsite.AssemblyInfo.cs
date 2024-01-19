f6f4ad4f42fecc83dad7e96a7ad6ffe5e628046b
AlbedoLinguine <yogevcr@gmail.com>
2024-01-19 16:18:04 -0600

    coommomomoit

--- obj/Debug/net8.0/weirdwebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/weirdwebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("weirdwebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4c5e3458d85961194197790f6018fcca6400830")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9f6dd4a247ddf2cb6efb96326d0c4f16ea752c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("weirdwebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("weirdwebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
